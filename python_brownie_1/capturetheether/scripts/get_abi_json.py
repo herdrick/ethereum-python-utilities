06a98cbc14829a5e47e708304050e865a8437807
Ethan Herdrick <herdrick@gmail.com>
2021-07-03 15:29:44 -1000

    foo

--- python_brownie_1/capturetheether/scripts/get_abi_json.py
+++ python_brownie_1/capturetheether/scripts/get_abi_json.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-# USAGE:
-#  MUST BE RUN with 'brownie run'
+# MUST be run with 'brownie run'
+# Example usage:
+#  export SOLIDITY_CLASS=CallMe7; brownie run scripts/get_abi_json.py > abis/CallMe7.json
 
 import json
 import os
