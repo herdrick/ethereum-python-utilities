1357f45ee90ab4ed108ac42b16f9021465174f24
Ethan Herdrick <herdrick@gmail.com>
2021-07-16 17:46:04 -1000

    invoke_function.py: --verbose flag

--- python_brownie_1/capturetheether/scripts/invoke_function.py
+++ python_brownie_1/capturetheether/scripts/invoke_function.py
@@ -7,12 +7,18 @@ from web3 import Web3
 import json
 import sys
 
+ABI_TYPE_TO_PYTHON_TYPE_MAPPING = {'string': str,
+                                   'bool': bool,
+                                   'int64': int,
+                                   'int64': int}
+
 @click.command()
 @click.argument('contract_address')
 @click.argument('function_name')
 @click.argument('abi_file', type=click.File('r'))
 @click.argument('args', nargs=-1)
-@click.option("--network", default="localhost", help="Ethereum network to use. If 'ropsten' or 'mainnet', use Infura, which requires the WEB3_INFURA_PROJECT_ID and WEB3_INFURA_API_SECRET env vars to be set. Default is 'localhost', ex. a local ganache node.")
+@click.option("--verbose", '-v', is_flag=True, default=False, help="Write verbose logging to stderr.")
+@click.option("--network", default="localhost", help="Ethereum network to use. If 'ropsten', 'rinkeby', or 'mainnet', use Infura, which requires the WEB3_INFURA_PROJECT_ID and WEB3_INFURA_API_SECRET env vars to be set. Default is 'localhost', which will connect to http://127.0.0.1:8545 i.e. a local ganache node.")
 @click.option("--transact/--call", default="False", help="""
 If '--transact', initiate a transaction which will be executed on the blockchain.
 
@@ -22,8 +28,8 @@ Default is '--call'""")
 @click.option('--ethereum_public_key', envvar='ETHEREUM_PUBLIC_KEY', help="Only used for --transact calls.")
 @click.option('--ethereum_private_key', envvar='ETHEREUM_PRIVATE_KEY', help="Only used for --transact calls.")
 @click.option("--nonce", type=click.INT, default=None, help="Only used for --transact calls. If not set, w3.eth.getTransactionCount will be called and its result used as the nonce.")
-def call_or_transact(contract_address, function_name, abi_file, args, network, transact, ethereum_public_key, ethereum_private_key, nonce):
-    """Call a function of a smart contract. You must have either an Infura account, if the function is on the mainnet or ropsten, or you must be running a local node (ex. using ganache-cli).
+def call_or_transact(contract_address, function_name, abi_file, args, verbose, network, transact, ethereum_public_key, ethereum_private_key, nonce):
+    """ Call a function of a smart contract. You must have either an Infura account, if the function is on the mainnet or ropsten, or you must be running a local node (ex. using ganache-cli).
 
     CONTRACT_ADDRESS: The address of the deployed contract.
 
@@ -42,7 +48,8 @@ def call_or_transact(contract_address, function_name, abi_file, args, network, t
     ... the json.dumps command returns a string which should go in the ABI_FILE.
 
     """
-    ic(contract_address, function_name, abi_file, args, network, transact, ethereum_public_key, ethereum_private_key, nonce)
+    if verbose:
+        ic(contract_address, function_name, abi_file, args, network, transact, ethereum_public_key, ethereum_private_key, nonce)
     if transact:
         assert ethereum_public_key and ethereum_private_key, "If you specify --transact then you must specify ethereum_private_key and ethereum_public_key or set those env vars."
     if network == 'localhost':
@@ -53,6 +60,11 @@ def call_or_transact(contract_address, function_name, abi_file, args, network, t
         from web3.auto.infura.rinkeby import w3
     if network == 'mainchain':
         from web3.auto.infura.mainnet import w3
+    if verbose:
+        ic(w3.api)
+        ic(w3.eth.protocol_version)
+        ic(w3)
+        ic(w3.isConnected())
     assert w3.isConnected()
     abi = json.load(abi_file)
     # cast args to types specified in abi
@@ -62,11 +74,17 @@ def call_or_transact(contract_address, function_name, abi_file, args, network, t
     if len(function_specs) < 1:
         raise ValueError(f"No function found in specified abi with name {function_name}.")
     function_spec = function_specs[0]
+    if verbose:
+        ic(function_spec)
     typed_args = []
     for arg, input_spec in zip(args, function_spec['inputs']):
         python_type = ABI_TYPE_TO_PYTHON_TYPE_MAPPING[input_spec['internalType']]
         typed_args.append(python_type(arg))
+    if verbose:
+        ic(typed_args)
     contract_instance = w3.eth.contract(address=contract_address, abi=abi)
+    if verbose:
+        ic(contract_instance.address)
     functions = contract_instance.all_functions()
     function = None
     for f in functions:
@@ -75,18 +93,24 @@ def call_or_transact(contract_address, function_name, abi_file, args, network, t
     if not function:
         click.echo(f"Specified function {function_name} does not exist in contract {contract_address}")
         sys.exit(1)
-    ic(function)
+    if verbose:
+        ic(function)
     if transact:
-        ic(ethereum_public_key, ethereum_private_key)
         if not nonce:
-            ic("no nonce specified; let's get it from w3.eth.getTransactionCount")
+            if verbose:
+                ic("no nonce specified; let's get it from w3.eth.getTransactionCount")
             nonce = w3.eth.getTransactionCount(ethereum_public_key)
-        ic(nonce)
+        if verbose:
+            ic(nonce)
+            ic(ethereum_public_key, ethereum_private_key)
         transaction_result = w3.eth.send_raw_transaction(w3.eth.account.sign_transaction(function().buildTransaction({'nonce': nonce}), ethereum_private_key).rawTransaction)
-        ic(transaction_result)
+        if verbose:
+            ic(transaction_result)
         click.echo(f"Success? Transaction address: {transaction_result.hex()}")
     else:
         call_result = function(*typed_args).call() # 'string1', 'string2'
+        if verbose:
+            ic(type(call_result))
         print(call_result)
 
 
